5e01ce917ed577fe1aa21c21b4874ca7c5c3d685
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2024-07-30 17:24:45 -0400

    set world and resources

--- src/Experience/World/World.js
+++ src/Experience/World/World.js
@@ -1,5 +1,6 @@
-import Experience from "./Experience";
+import Experience from "../Experience";
 import * as THREE from 'three'
+import Environment from "./Environment";
 
 export default class World {
   constructor(){
@@ -8,8 +9,11 @@ export default class World {
 
     const testMesh = new THREE.Mesh(
       new THREE.BoxGeometry(1,1,1),
-      new THREE.MeshBasicMaterial({wireframe: true})
+      new THREE.MeshStandardMaterial()
     )
     this.scene.add(testMesh)
+
+    // Setup
+    this.environment = new Environment()
   }
 }
