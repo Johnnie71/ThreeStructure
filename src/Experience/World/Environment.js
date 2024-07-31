79d02c9d6ee0b91ee07229626dbf1d86415f5904
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2024-07-31 12:30:45 -0400

    add: environment debugging

--- src/Experience/World/Environment.js
+++ src/Experience/World/Environment.js
@@ -6,6 +6,12 @@ export default class Environment {
     this.experience = new Experience()
     this.scene = this.experience.scene
     this.resources = this.experience.resources
+    this.debug = this.experience.debug
+
+    // Debug
+    if (this.debug.active) {
+      this.debugFolder = this.debug.ui.addFolder('environment')
+    }
   
 
     // Setup
@@ -42,5 +48,35 @@ export default class Environment {
     }
 
     this.setEnvironmentMap.updateMaterial()
+
+    if(this.debug.active){
+      this.debugFolder
+          .add(this.sunLight, 'intensity')
+          .name('sunLightIntensity')
+          .min(0)
+          .max(10)
+          .step(0.001)
+      
+      this.debugFolder
+          .add(this.sunLight.position, 'x')
+          .name('sunLightX')
+          .min(- 5)
+          .max(5)
+          .step(0.001)
+      
+      this.debugFolder
+          .add(this.sunLight.position, 'y')
+          .name('sunLightY')
+          .min(- 5)
+          .max(5)
+          .step(0.001)
+      
+      this.debugFolder
+          .add(this.sunLight.position, 'z')
+          .name('sunLightZ')
+          .min(- 5)
+          .max(5)
+          .step(0.001)
+      }
   }
 }
