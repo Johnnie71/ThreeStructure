bd8e71f1e5e1436f037cfd65fccaabca25ce66b9
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2024-07-31 11:42:29 -0400

    add: added fox with shadow to world

--- src/Experience/World/World.js
+++ src/Experience/World/World.js
@@ -1,7 +1,7 @@
 import Experience from "../Experience";
-import * as THREE from 'three'
 import Environment from "./Environment";
 import Floor from "./Floor";
+import Fox from "./Fox";
 
 export default class World {
   constructor(){
@@ -9,17 +9,12 @@ export default class World {
     this.scene = this.experience.scene
     this.resources = this.experience.resources
 
-    const testMesh = new THREE.Mesh(
-      new THREE.BoxGeometry(1,1,1),
-      new THREE.MeshStandardMaterial()
-    )
-    this.scene.add(testMesh)
-
     // Wait for resources
     this.resources.on('ready', () => {
       // Setup
-      this.environment = new Environment()
       this.floor = new Floor()
+      this.fox = new Fox()
+      this.environment = new Environment()
     })
   }
 }
